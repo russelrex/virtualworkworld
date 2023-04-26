229b200a4e1f970aa910f1dd59c56da254b0a5ce
russelrex <russelrex_23@yahoo.com>
2023-04-26 16:52:13 +0800

    Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
-import { TestimonialsComponent } from './component/testimonials/testimonials.component';
-import { CandidatesComponent } from './component/candidates/candidates.component';
-import { SearchComponent } from './component/candidates/search/search.component';
-import { InfoComponent } from './component/candidates/info/info.component';
-import { JobsCandidateComponent } from './component/candidates/jobs-candidate/jobs-candidate.component';
-import { EmployersComponent } from './component/employers/employers.component';
-import { EmployersInfoComponent } from './component/employers/employers-info/employers-info.component';
-import { EmployersServicesComponent } from './component/employers/employers-services/employers-services.component';
-import { EmployersPricingComponent } from './component/employers/employers-pricing/employers-pricing.component';
 import { AboutComponent } from './component/about/about.component';
-import { BlogComponent } from './component/blog/blog.component';
-import { BlogInfoComponent } from './component/blog/blog-info/blog-info.component';
-import { BlogFullComponent } from './component/blog/blog-full/blog-full.component';
-import { BlogListsComponent } from './component/blog/blog-lists/blog-lists.component';
 import { ContactComponent } from './component/contact/contact.component';
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
-import { CorporateComponent } from './component/corporate/corporate.component';
-import { ComingSoonComponent } from './component/coming-soon/coming-soon.component';
-import { NgMarqueeModule } from 'ng-marquee';
-import { AccountingComponent } from './component/jobs/accounting/accounting.component';
 import { JobsComponent } from './component/jobs/jobs.component';
-import { TestimonialsComponent } from './component/testimonials/testimonials.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { InfoComponent } from './component/candidates/info/info.component';
 import { SearchComponent } from './component/candidates/search/search.component';
-import { EmployersComponent } from './component/employers/employers.component';
 import { EmployersInfoComponent } from './component/employers/employers-info/employers-info.component';
 import { EmployersPricingComponent } from './component/employers/employers-pricing/employers-pricing.component';
 import { EmployersServicesComponent } from './component/employers/employers-services/employers-services.component';
 import { AboutComponent } from './component/about/about.component';
-import { BlogComponent } from './component/blog/blog.component';
-import { BlogInfoComponent } from './component/blog/blog-info/blog-info.component';
 import { ContactComponent } from './component/contact/contact.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -28,20 +20,13 @@ import { FooterComponent } from './footer/footer.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    CorporateComponent,
-    ComingSoonComponent,
-    AccountingComponent,
     JobsComponent,
-    TestimonialsComponent,
     InfoComponent,
     SearchComponent,
-    EmployersComponent,
     EmployersInfoComponent,
     EmployersPricingComponent,
     EmployersServicesComponent,
     AboutComponent,
-    BlogComponent,
-    BlogInfoComponent,
     ContactComponent,
     HeaderComponent,
     FooterComponent,
@@ -49,7 +34,6 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgMarqueeModule,
     NgbModule
   ],
   providers: [],
